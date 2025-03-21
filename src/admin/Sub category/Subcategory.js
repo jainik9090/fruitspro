0bbcb18ecc6e4685e9692d780db316bd22d82270
jainik9090 <jainikkorat@gmail.com>
2025-03-21 18:01:28 +0530

    erdgh

--- src/admin/Sub category/Subcategory.js
+++ src/admin/Sub category/Subcategory.js
@@ -6,17 +6,38 @@ import {
   DialogContent,
   DialogTitle,
   FormControl,
+  FormHelperText,
+  IconButton,
   InputLabel,
   MenuItem,
+  NativeSelect,
+  Paper,
   Select,
   TextField,
 } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { useFormik } from "formik";
-import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import React, { useEffect, useState } from "react";
 import { object, string } from "yup";
 
 function Subcategory(props) {
   const [open, setOpen] = React.useState(false);
+  const [data, setCategorydata] = React.useState([]);
+  const [subd, setSubd] = useState([]);
+  const [update, setUpdate] = useState([]);
+
+  useEffect(() => {
+    getcategorydata();
+    getData();
+  }, []);
+
+  const getcategorydata = () => {
+    const localdata = JSON.parse(localStorage.getItem("category"));
+    setCategorydata(localdata);
+  }
+
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -24,23 +45,93 @@ function Subcategory(props) {
 
   const handleClose = () => {
     setOpen(false);
+    setOpen(false);
+    resetForm();
   };
   const Subcategoryyup = object({
+    Category: string().required(),
     SubCategory: string().required(),
     Descripition: string().required(),
   });
 
   const formikcat = useFormik({
     initialValues: {
+      Category: "",
       SubCategory: "",
       Descripition: "",
     },
     validationSchema: Subcategoryyup,
-    onSubmit: (values) => {
+    onSubmit: (values, {resetForm}) => {
       console.log(values);
+      
+      const subdata = JSON.parse(localStorage.getItem("subcategory"))
+
+     if (update) {
+      let index = subdata.findIndex((v) => v.id === values.id);
+      console.log(index);
+      subdata[index] = values;
+      localStorage.setItem("subcategory", JSON.stringify(subdata));
+     } else {
+      let obj = {...values, id:Math.floor(Math.random() *1000)};
+      if (subdata) {
+        subdata.push(obj);
+          localStorage.setItem("subcategory", JSON.stringify(subdata));
+      } else {
+        localStorage.setItem("subcategory", JSON.stringify([obj]));
+      }
+     }
+
+      getData();
+      handleClose();
+      resetForm();
+    
     },
   });
-  const { handleSubmit, handleBlur, handleChange, values, errors, touched } =
+
+  const handleDelete = (id) => {
+    console.log(id);
+    const subdata = subd.filter((v) => v.id !== id);
+    localStorage.setItem("subcategory", JSON.stringify(subdata));
+    getData();
+    handleClose();
+  }
+
+
+  const handleEdit = (data) => {
+    console.log(data);
+    setValues(data)
+    handleClickOpen();
+    setUpdate(true);
+  }
+
+  const columns = [
+    { field: "Category", headerName: "Category", width: 70 },
+    { field: "SubCategory", headerName: "SubCategory", width: 130 },
+    { field: "Descripition", headerName: "Descripition", width: 130 },
+    {
+      headerName: "Action",
+      renderCell: (params) => (
+        <>
+          <IconButton aria-label="edit" onClick={() => handleEdit(params.row)}>
+            <EditIcon />
+          </IconButton>
+          <IconButton aria-label="delete" onClick={() => handleDelete(params.row.id)}>
+            <DeleteIcon />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
+
+
+  const getData = () => {
+    const subdata = JSON.parse(localStorage.getItem("subcategory"))
+    
+    setSubd(subdata);
+  }
+  const paginationModel = { page: 0, pageSize: 5 };
+
+  const { handleSubmit, handleBlur, handleChange, values, errors, touched ,resetForm, setValues} =
     formikcat;
 
   console.log(values);
@@ -54,37 +145,29 @@ function Subcategory(props) {
       <Dialog
         open={open}
         onClose={handleClose}
-        slotProps={{
-          paper: {
-            component: "form",
-            onSubmit: (event) => {
-              event.preventDefault();
-              const formData = new FormData(event.currentTarget);
-              const formJson = Object.fromEntries(formData.entries());
-              const email = formJson.email;
-              console.log(email);
-              handleClose();
-            },
-          },
-        }}
       >
         <DialogTitle>Sub Category</DialogTitle>
 
         <form onSubmit={handleSubmit}>
-          <FormControl sx={{m: 2, minWidth:120}} size="small">
-            <InputLabel id="demo-simple-select-label">Category</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={values.Category}
-              label="Category Name"
+           <FormControl sx={{ m: 3, minWidth: 120 }} error={touched.Category && errors.Category}>
+            <NativeSelect
+              defaultValue={30}
+              inputProps={{
+                name: 'Category',
+                id: 'uncontrolled-native',
+              }}
               onChange={handleChange}
               onBlur={handleBlur}
+              value={values.Category}
             >
-              <MenuItem value={1}>{values.Category}</MenuItem>
-        
-        
-            </Select>
+              <option value="">--Select Category--</option>
+              {
+                data?.map((v) => (
+                  <option value={v.id}>{v.Category}</option>
+                ))
+              }
+            </NativeSelect>
+            <FormHelperText>{ touched.Category && errors.Category? errors.Category:""}</FormHelperText>
           </FormControl>
           <DialogContent>
             <TextField
@@ -128,6 +211,16 @@ function Subcategory(props) {
           </DialogActions>
         </form>
       </Dialog>
+      <Paper sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={subd}
+          columns={columns}
+          initialState={{ pagination: { paginationModel } }}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+          sx={{ border: 0 }}
+        />
+      </Paper>
     </React.Fragment>
   );
 }
